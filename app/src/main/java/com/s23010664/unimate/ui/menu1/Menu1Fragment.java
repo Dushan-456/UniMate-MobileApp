073ec0e0a44f580a0b85025c2e5cbbc0da3290a7
Navodya Dushan <168759740+Dushan-456@users.noreply.github.com>
2025-06-18 01:37:28 +0530

    Update Menu1Fragment.java

--- app/src/main/java/com/s23010664/unimate/ui/menu1/Menu1Fragment.java
+++ app/src/main/java/com/s23010664/unimate/ui/menu1/Menu1Fragment.java
@@ -19,24 +19,15 @@ import com.s23010664.unimate.ui.slideshow.SlideshowViewModel;
 
 public class Menu1Fragment extends Fragment {
 
-    private Menu1ViewModel Menu1ViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        Menu1ViewModel Menu1ViewModel =
-                new ViewModelProvider(this).get(Menu1ViewModel.class);
+
 
         @NonNull FragmentMenu1Binding binding = FragmentMenu1Binding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textMenu;
-        Menu1ViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
-    }
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        Object binding = null;
     }
 
 
