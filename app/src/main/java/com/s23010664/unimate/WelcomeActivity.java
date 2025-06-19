7bc82ccd32b52ef246fa92755b4315cad8a8106f
Navodya Dushan <168759740+Dushan-456@users.noreply.github.com>
2025-06-20 01:51:17 +0530

    UPDATED

--- app/src/main/java/com/s23010664/unimate/WelcomeActivity.java
+++ app/src/main/java/com/s23010664/unimate/WelcomeActivity.java
@@ -2,7 +2,10 @@ package com.s23010664.unimate;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class WelcomeActivity extends AppCompatActivity {
 
@@ -10,5 +13,29 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        //Login Button
+        Button login_btn = findViewById(R.id.btnLogin);
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent loginIntent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(loginIntent);
+
+            }
+        });
+
+        //Registration
+        Button reg_btn = findViewById(R.id.btnRegister);
+        reg_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent registerIntent = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(registerIntent);
+
+            }
+        });
+
+
     }
 }
