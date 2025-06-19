a00bb368f1a36ee868336ca846232b81dfa435d9
Navodya Dushan <168759740+Dushan-456@users.noreply.github.com>
2025-06-20 02:08:38 +0530

    Update RegisterActivity.java

--- app/src/main/java/com/s23010664/unimate/RegisterActivity.java
+++ app/src/main/java/com/s23010664/unimate/RegisterActivity.java
@@ -2,7 +2,11 @@ package com.s23010664.unimate;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        //Login Button
+        TextView login_btn = findViewById(R.id.toLogin);
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent loginIntent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(loginIntent);
+
+            }
+        });
     }
 }
