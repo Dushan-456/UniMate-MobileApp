d317344f7d47607a68678a8a8e2b81b946d93564
Navodya Dushan <168759740+Dushan-456@users.noreply.github.com>
2025-06-19 17:50:15 +0530

    Activity List Create

---
+++ app/src/main/java/com/s23010664/unimate/ui/activitylist/ActivityListFragment.java
@@ -0,0 +1,38 @@
+package com.s23010664.unimate.ui.activitylist;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.s23010664.unimate.R;
+
+public class ActivityListFragment extends Fragment {
+
+    private ActivityListViewModel mViewModel;
+
+    public static ActivityListFragment newInstance() {
+        return new ActivityListFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_activity_list, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(ActivityListViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
