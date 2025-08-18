7f4caad27df9d683b2d99edeaf216f138d77f86e
Navodya Dushan <168759740+Dushan-456@users.noreply.github.com>
2025-08-18 12:07:17 +0530

    Create AddFoundItemFragment.java

---
+++ app/src/main/java/com/s23010664/unimate/ui/addfounditem/AddFoundItemFragment.java
@@ -0,0 +1,32 @@
+package com.s23010664.unimate.ui.addfounditem;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.s23010664.unimate.R;
+
+public class AddFoundItemFragment extends Fragment {
+
+
+    public static AddFoundItemFragment newInstance() {
+        return new AddFoundItemFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_add_found_item, container, false);
+    }
+
+
+
+}
